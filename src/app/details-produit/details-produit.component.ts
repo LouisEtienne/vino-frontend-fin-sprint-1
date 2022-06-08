0b1f331bef399f1622bb577d3e88ab7fa302b624
DmitriDV <ddudchenko@gmail.com>
2022-06-07 23:11:22 -0400

    apibiero.service. Les commentaires sont ajoutés.

--- src/app/details-produit/details-produit.component.ts
+++ src/app/details-produit/details-produit.component.ts
@@ -3,22 +3,22 @@ import { ActivatedRoute } from '@angular/router';
 import { ApibieroService } from '../Serv/apibiero.service';
 
 @Component({
-  selector: 'app-details-produit',
-  templateUrl: './details-produit.component.html',
-  styleUrls: ['./details-produit.component.scss']
+    selector: 'app-details-produit',
+    templateUrl: './details-produit.component.html',
+    styleUrls: ['./details-produit.component.scss']
 })
 export class DetailsProduitComponent implements OnInit {
   
-  constructor(private route:ActivatedRoute, private apibiero:ApibieroService) { }
+    constructor(private route:ActivatedRoute, private apibiero:ApibieroService) { }
 
-  ngOnInit(): void {
-    this.route.params.subscribe(params=>{
-      console.log(params['id']);
-      this.apibiero.getBiere(params['id']).subscribe(biere=>{
-        console.log(biere);
-      })
-      // Faire une requête avec le apibieroServ
-    })
-  }
+    ngOnInit(): void {
+        this.route.params.subscribe(params=>{
+        console.log(params['id']);
+        this.apibiero.getBouteille(params['id']).subscribe(biere=>{
+            console.log(biere);
+        })
+        // Faire une requête avec le apibieroServ
+        })
+    }
 
 }
